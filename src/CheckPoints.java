fdf7bfc732f70d3d4cdd5ad5dee4f846de637cb0
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-23 15:18:10 -0300

    Separating the CheckPoints class

--- src/CheckPoints.java
+++ src/CheckPoints.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CheckPoints {
+public class CheckPoints {
+  public CheckPoints (int points) {
+	if (points <= 0) {
+	  System.out.println("Your answer was incorrect!");
+	  System.out.println("Your total points are: " + points);
+	} else {
+	  points--;
+	  System.out.println("Your answer was incorrect!" + "You have lost a point!");
+	  System.out.println("Your total points are: " + points);
+	}
+  }
 }
+
