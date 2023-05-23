1e9cf14f35ec0e5fb6f740a2d534b43fe9cb1495
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-23 15:17:05 -0300

    Adding main functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main (String[] args) {
+	int x = 5;
+	int y = 7;
+	int z = 10;
+	int points = 0;
+
+	Scanner scanner = new Scanner(System.in);
+
+	//	First question
+	System.out.println("First question");
+	String question_1 = "How much is: " + x + "*" + y;
+	System.out.println(question_1);
+	int userInput = scanner.nextInt();
+
+	if (userInput == x * y) {
+	  points++;
+	  System.out.println("Your answer was correct!" + " You have gained a point!");
+	  System.out.println("Your total points are: " + points);
+	} else {
+	  new CheckPoints(points);
+	  System.out.println("The correct answer was: " + x * y);
+	}
+	System.out.println(" ");
+
+	//	Second question
+	System.out.println("Second question");
+	String question_2 = "How much is: " + z + "*" + y;
+	System.out.println(question_2);
+	userInput = scanner.nextInt();
+
+	if (userInput == z * y) {
+	  points++;
+	  System.out.println("Your answer was correct!" + " You have gained a point!");
+	  System.out.println("Your total points are: " + points);
+	} else {
+	  new CheckPoints(points);
+	  System.out.println("The correct answer was: " + z * y);
+	}
+	System.out.println(" ");
+
+	//	Third question
+	System.out.println("Third question");
+	String question_3 = "How much is: " + x + "/" + z;
+	System.out.println(question_3);
+	userInput = scanner.nextInt();
+
+	if (userInput == x / y) {
+	  points++;
+	  System.out.println("Your answer was correct!" + " You have gained a point!");
+	  System.out.println("Your total points are: " + points);
+	} else {
+	  new CheckPoints(points);
+	  System.out.println("The correct answer was: " + x / z);
+	}
+
+	System.out.println(" ");
+	System.out.println("The game is over!");
+	if (points <= 0) {
+	  System.out.println("You Finished the game with: " + points + " points!");
+	  System.out.println("You need to practice a little more math and try again");
+	} else if (points >= 1 && points < 3) {
+	  System.out.println("Congrats!");
+	  System.out.println("You Finished the game with: " + points + " points!");
+	} else if (points == 3) {
+	  System.out.println("Congrats!");
+	  System.out.println("You Finished the game with: " + points + " points!");
+	  System.out.println("You are a math god!");
+	}
+  }
 }
+
